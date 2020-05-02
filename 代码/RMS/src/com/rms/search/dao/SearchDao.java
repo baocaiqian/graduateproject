ddae8290a525066b253023a950a651e71e4e9d97
baocaiqian <2952635041@qq.com>
2020-05-03 00:14:39 +0800

    搜索词

--- 代码/RMS/src/com/rms/search/dao/SearchDao.java
+++ 代码/RMS/src/com/rms/search/dao/SearchDao.java
@@ -1,8 +1,23 @@
 package com.rms.search.dao;
 
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class SearchDao {
-
+	@Resource
+	private SessionFactory sf;
+	//获取热门搜索词汇
+	public List<String> getHotSearch() {
+		Session session = sf.getCurrentSession();
+		String sql="select new List(s.title,count(*)) form Search s group by s.title";
+		List list = session.createQuery(sql).list();
+		
+		return null;
+	}
 }
